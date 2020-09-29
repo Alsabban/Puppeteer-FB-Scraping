e9642472b2f5dcd750c4824c8424d91fd38b0673
be320 <ahmedbahaa.3@hotmail.com>
2020-09-29 20:49:43 +0200

    Code Explained

--- index.js
+++ index.js
@@ -1,222 +1,150 @@
-const puppeteer = require('puppeteer');
-const dotenv = require('dotenv');
-const fs = require('fs');
+//imports
+const puppeteer = require("puppeteer");
+const dotenv = require("dotenv");
+const fs = require("fs");
 dotenv.config();
+
+//GLOBAL VARIABLES
 const WAIT_FOR_PAGE = 5000;
 const DELAY_USER_INPUT = 500;
 const DELAY_PW_INPUT = 500;
 
+//Main Function
 (async () => {
-    try{
-        const browser = await puppeteer.launch({headless: false});
-        const context = browser.defaultBrowserContext();
-        await context.overridePermissions(process.env.FB_PAGE, ['notifications']);
-        const page = await browser.newPage();
-        await page.goto(process.env.FB_PAGE);
-        await delay(WAIT_FOR_PAGE);  
-        if (await page.$('div[role="dialog"]') !== null){
-          await page.waitForSelector('div[role="dialog"]');  
-          await page.click('div[role="button"]') ;
-        }
-        await page.waitForSelector('input[name="email"]');
-        await page.type('input[name="email"]', process.env.FB_USER, {delay: DELAY_USER_INPUT});
-        await page.type('input[name="pass"]', process.env.FB_PW, {delay: DELAY_PW_INPUT});
-        await page.click('div[aria-label="Accessible login button"]');
-        await page.waitForNavigation(".userContentWrapper");
-        await delay(WAIT_FOR_PAGE);
-        const data = await page.evaluate(async function (){
-            var postList = [];
-            async function scrapData() {
-            try{
-              const postListLength = document.querySelectorAll('.userContentWrapper').length;
-              console.log('postListLength ', postListLength);
-              class Post {
-                constructor(document) {
-                   console.log("post initiallized");
-                    this.document = document;
-                    this.post = document.querySelector('.userContentWrapper');
-                }
-            
-                get PostLength() {
-                    try {
-                      return this.document.querySelectorAll('.userContentWrapper').length;
-                    } catch (error) {
-                      console.log('PostLength error ===> ', error);
-                    }
-                }
-            
-                PressSeeMore() {
-                    try {
-                      return new Promise((resolve, reject) => {
-                        if(this.post && this.post.innerText.includes('See More')) {
-                            this.post.querySelector('span[class=see_more_link_inner]').click();
-                        }
-                        setTimeout(() => {
-                            return resolve();
-                        }, 1000);
-                      })               
-                    } catch (error) {
-                      console.log('post PressSeeMore error ===> ', error);
-                    }
-                }
-            
-                scrap() {
-                  try {
-                    return new Promise((resolve) => {
-                      setTimeout(() => {
-                        return resolve(this.post.querySelector('div[data-testid="post_message"').innerText.trim());
-                      }, 1000);
-                    });
-                  } catch (error) {
-                    console.log('scrap post error ===> ', error);
-                  }
-              }
-      
-            
-                delete() {
-                    this.post.remove();
-                }
-              }
+  try {
+    //Running Chrome
+    const browser = await puppeteer.launch({ headless: false });
+    const context = browser.defaultBrowserContext();
+    //Opening the Facebook Group
+    await context.overridePermissions(process.env.FB_PAGE, ["notifications"]);
+    const page = await browser.newPage();
+    await page.goto(process.env.FB_PAGE);
+    await delay(WAIT_FOR_PAGE);
+    //Closing popup Dialog
+    if ((await page.$('div[role="dialog"]')) !== null) {
+      await page.waitForSelector('div[role="dialog"]');
+      await page.click('div[role="button"]');
+    }
+    //Login by entering email and password
+    await page.waitForSelector('input[name="email"]');
+    await page.type('input[name="email"]', process.env.FB_USER, {
+      delay: DELAY_USER_INPUT
+    });
+    await page.type('input[name="pass"]', process.env.FB_PW, {
+      delay: DELAY_PW_INPUT
+    });
+    await page.click('div[aria-label="Accessible login button"]');
+    //Wait for redirect to the group after login
+    await page.waitForNavigation(".userContentWrapper");
+    await delay(WAIT_FOR_PAGE);
 
-              class Comment {
-                constructor(post) {
-                    this.comment = post.post.querySelector('div[data-testid="UFI2CommentsList/root_depth_0"]');
-                    this.post = post.post;
-                }
-                
-                get commentLength() {
-                    try {
-                      return this.post.querySelectorAll('div[aria-label="Comment"] div[data-testid="UFI2Comment/body"]').length;
-                    } catch (error) {
-                      console.log('commentLength error ===>', error);
-                    }
-                }
-                
-                PressSeeMoreComments() {
-                    try {
-                      return new Promise((resolve, reject) => {
-                        if(this.comment.innerText.includes('View')) {
-                            this.comment.querySelector('span').click();
-                        }
-                        setTimeout(() => {
-                            return resolve();
-                        }, 1000);
-                      });                
-                    } catch (error) {
-                      console.log('PressSeeMoreComments error ==> ', error);
-                    }
-                }
-                
-                pressMoreReplies() {
-                    try {
-                      return new Promise((resolve, reject) => {
-                        [...this.comment.querySelectorAll('span')]
-                          .forEach(span => {
-                              if(span.innerText.includes('replies')) {
-                              span.click();
-                              }
-                          });
-                          setTimeout(() => {
-                              return resolve();
-                          }, 1000);
-                      })                
-                    } catch (error) {
-                      console.log('pressMoreReplies error ==> ', error);
-                    }
-                }
-                
-                pressSeeMoreForAllComments() {
-                    try {
-                      return new Promise((resolve, reject) => {
-                        [...this.comment.querySelectorAll('span')]
-                          .forEach(i => {
-                            // console.log('element to press see more on all comments ', i)
-                            // console.log('element text to press see more on all comments ', i.innerText)
-                            // console.log('is element to press see more on all comments? ', i.innerText.includes('See more'))
-                              if(i.innerText.includes('See more')) {
-                                // console.log('i.querySelector(\'a\')', i.querySelector('a'));
-                                i.click();
-                              }
-                          })
-                          setTimeout(() => {
-                              return resolve();
-                          }, 1000);
-                      })
-                    } catch (error) {
-                      console.log('pressSeeMoreForAllComments error ==>', error);
-                    }
-                }
-                
-                scrap() {
-                    try {
-                      return new Promise( resolve => {
-                        setTimeout(() => {
-                          return resolve([...this.comment.querySelectorAll('span[dir="ltr"]'), ...this.comment.querySelectorAll('span[dir="rtl"]')]
-                          .map(item => item.innerText));
-                        }, 1000);
-                      })
-                    } catch (error) {
-                      console.log('scrap comments error ===> ', error);
-                    }
-                }
-                
-                delete() {
-                    this.comment.remove();
-                }
-              }
-            
-              const post = new Post(document);
-              if(post.post) {
-                await post.PressSeeMore();
-                //comment = new Comment(post);
-                //await comment.PressSeeMoreComments();      
-                //await comment.pressMoreReplies();
-                //await comment.pressSeeMoreForAllComments();
-                // console.log('posts = ', post.scrap());
-                // console.log('comments = ',  comment.scrap());
-                postList.push({ post: await post.scrap()});
-                console.log('Data now ====> ', postList);
-                post.delete();
-                // console.log('Current Array ===> ', postList);
-                setTimeout(() => window.scrollBy(0, 100), 1000);
-                if(postList.length<50) await scrapData();  //if(postListLength*1)
-                else return postList;
-              } else {
-                console.log('postList if no post found ==> ', postList);
-                return postList;
-              };
-                }catch (error) {
-                    console.error('error from scrapDataFunction ==>', error);
-                    debugger;
-                  }
+    //The Process of Retrieving Data
+    const data = await page.evaluate(async function() {
+      var postList = []; //Empty List of Posts
+
+      //Scraping Data Function
+      async function scrapData() {
+        try {
+          // Detecting the number of the posts loaded on the browser
+          const postListLength = document.querySelectorAll(
+            ".userContentWrapper"
+          ).length;
+          console.log("postListLength ", postListLength);
+
+          //Post Class
+          class Post {
+
+            //Post Constructor
+            constructor(document) {
+              console.log("post initiallized");
+              this.document = document;
+              this.post = document.querySelector(".userContentWrapper");
             }
-            await scrapData();
-            return await Promise.all(postList);
-        });
-        
-        await storeDataToFile('./fb.json', data);
-    }catch (error) {
-        console.log('Catched error message', error.message);
-        console.log('Catched error stack', error.stack);
-        console.log('Catched error ', error);
+
+            //Click See More to get the full post text
+            PressSeeMore() {
+              try {
+                return new Promise((resolve, reject) => {
+                  if (this.post && this.post.innerText.includes("See More")) {
+                    this.post
+                      .querySelector("span[class=see_more_link_inner]")
+                      .click();
+                  }
+                  setTimeout(() => {
+                    return resolve();
+                  }, 1000);
+                });
+              } catch (error) {
+                console.log("post PressSeeMore error ===> ", error);
+              }
+            }
+
+            //Retrieve the text from the post
+            scrap() {
+              try {
+                return new Promise(resolve => {
+                  setTimeout(() => {
+                    return resolve(
+                      this.post
+                        .querySelector('div[data-testid="post_message"')
+                        .innerText.trim()
+                    );
+                  }, 1000);
+                });
+              } catch (error) {
+                console.log("scrap post error ===> ", error);
+              }
+            }
+
+            //to delete the post
+            delete() {
+              this.post.remove();
+            }
+          }
+
+          const post = new Post(document);
+          if (post.post) {
+            await post.PressSeeMore();
+            postList.push(await post.scrap());
+            console.log("Data now ====> ", postList);
+            post.delete();
+            setTimeout(() => window.scrollBy(0, 100), 1000);
+            if (postList.length < 5) await scrapData(); //if(postListLength*1)  //load 5 posts only
+            else return postList;
+          } else {
+            console.log("postList if no post found ==> ", postList);
+            return postList;
+          }
+        } catch (error) {
+          console.error("error from scrapDataFunction ==>", error);
+          debugger;
+        }
       }
+      await scrapData();
+      return await Promise.all(postList);
+    });
+
+    await storeDataToFile("./fb.json", data);
+  } catch (error) {
+    console.log("Catched error message", error.message);
+    console.log("Catched error stack", error.stack);
+    console.log("Catched error ", error);
+  }
 })();
 
+//Storig data into json file
 const storeDataToFile = async function(file, data) {
-    console.log(data);
-    return fs.writeFileSync(file, JSON.stringify(data), err => {
-      if (err) {
-        return err;
-      }
-      return;
-    });
-  };
+  console.log(data);
+  return fs.writeFileSync(file, JSON.stringify(data), err => {
+    if (err) {
+      return err;
+    }
+    return;
+  });
+};
 
-  function delay(time) {
-    return new Promise(function(resolve) { 
-        setTimeout(resolve, time)
-    });
-  }
-
-
-
+function delay(time) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, time);
+  });
+}
