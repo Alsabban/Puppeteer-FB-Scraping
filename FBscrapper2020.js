84fa92174aa82b77bdc72f6fe3133b50c59048e3
Youssef <youssef.ossama2@gmail.com>
2020-12-22 16:05:55 +0200

    script gets one post

--- FBscrapper2020.js
+++ FBscrapper2020.js
@@ -5,64 +5,119 @@ const fs = require("fs");
 dotenv.config();
 
 //GLOBAL VARIABLES
-const WAIT_FOR_PAGE = 3000;
-const DELAY_USER_INPUT = 0;
-const DELAY_PW_INPUT = 0;
-
-
+const WAIT_FOR_PAGE = 1000;
+const DELAY_INPUT = 1;
 
 //Main Function
 (async () => {
-    try {
-        //Running Chrome
-    const browser = await puppeteer.launch({ executablePath: process.env.CHROME_PATH, headless:false });
+  try {
+    //starting Chrome
+    const browser = await puppeteer.launch({
+      executablePath: process.env.CHROME_PATH,
+      headless: false,
+    });
     const context = browser.defaultBrowserContext();
-    //Opening the Facebook Login
     await context.overridePermissions(process.env.FB_LOGIN, ["notifications"]);
-    const page = await browser.newPage({ viewport: null });
     
-    //await page.setViewport(null);
+    //Opening the Facebook Login
+    const page = await browser.newPage({ viewport: null });
     await page.goto(process.env.FB_LOGIN);
     await delay(WAIT_FOR_PAGE);
+    
+    //logging in
     await page.waitForSelector('input[name="email"]');
     await page.type('input[name="email"]', process.env.FB_USER, {
-     delay: DELAY_USER_INPUT
+      delay: DELAY_INPUT,
     });
     await page.type('input[name="pass"]', process.env.FB_PW, {
-     delay: DELAY_PW_INPUT
+      delay: DELAY_INPUT,
     });
-    await page.click('button[data-testid="royal_login_button"]');
-   await delay(WAIT_FOR_PAGE);
+    await Promise.all([
+      await page.click('button[data-testid="royal_login_button"]'),
+      page.waitForNavigation({ waitUntil: 'networkidle0' })
+    ]);
+    //await page.click('button[data-testid="royal_login_button"]');
+    //await delay(5*WAIT_FOR_PAGE);
+
     //Opening the Facebook Group
-     await page.goto(process.env.FB_PAGE);
-     await page.waitForNavigation({ waitUntil: 'networkidle0' });
-    
+    await page.goto(process.env.FB_PAGE);
+    await delay(WAIT_FOR_PAGE);
 
-
-
-
-} catch (error) {
-  console.log("Catched error message", error.message);
-  console.log("Catched error stack", error.stack);
-  console.log("Catched error ", error);
-}
+    //scraping function
+    const posts = await page.evaluate(async () => {
+      let posts = [],
+      postcounter = 0,
+      NUMBER_OF_POSTS = 1;
+      class Post {
+        constructor(document) {
+          //this.document = document;
+          this.postdata = document.querySelector('div[data-ad-preview="message"]'); //getting post
+        }
+        getpostfeatures() {
+          try {
+            return new Promise((resolve) => {
+              //gettin the features we are intrested in from the post
+              setTimeout(() => {
+                if (this.postdata.querySelector("span") === null) {
+                  return resolve("");
+                } else {
+                  return resolve(
+                    this.postdata.querySelector("span").innerText.trim()
+                  );
+                }
+              }, 0);
+            });
+          } catch (error) {
+            console.log("scrap post error ===> ", error);
+          }
+        }
+        //to remove the post
+        removepost() {
+          this.postdata.remove();
+        }
+      } //end of post class
+      try {
+        while (postcounter < NUMBER_OF_POSTS) {
+          const post = new Post(document);
+          if (post.postdata) {
+            postcounter++;
+            let mydata = await post.getpostfeatures();
+            posts.push({
+              post_id: postcounter,
+              post: mydata
+            });
+            console.log(mydata);
+            post.removepost();
+          }
+        }
+      } catch (error) {
+        console.error("error while getting newspage!!", error);
+      }
+      return posts;
+    });
+    //storing the posts we scraped in a json file
+    storeDataInJSON("./EducationNews.json", posts);
+    await browser.close();
+  } catch (error) {
+    console.log("Catched error message", error.message);
+    console.log("Catched error stack", error.stack);
+    console.log("Catched error ", error);
+  }
 })();
 
-
 //Storig data into json file
-const storeDataInJSON = async function(file, data) {
-    console.log(data);
-    return fs.writeFileSync(file, JSON.stringify(data), err => {
-      if (err) {
-        return err;
-      }
-      return;
-    });
-  };
+const storeDataInJSON = async function (file, data) {
+  console.log(data);
+  return fs.writeFileSync(file, JSON.stringify(data), (err) => {
+    if (err) {
+      return err;
+    }
+    return;
+  });
+};
 
-  function delay(time) {
-    return new Promise(function(resolve) {
-      setTimeout(resolve, time);
-    });
-  }
-  
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
